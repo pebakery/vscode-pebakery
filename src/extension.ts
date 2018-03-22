43621adc1be325d2da7e5a931e2ac8d678a3acab
Hajin Jang <jb6804@naver.com>
2018-03-23 02:15:42 +0900

    Disable editor.wordBasedSuggestions in .script

---
+++ src/extension.ts
@@ -0,0 +1,18 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    // Disable editor.wordBasedSuggestions, or encoded file will disrupt intellisense
+    const config = vscode.workspace.getConfiguration();
+    config.update("[pebakery]", {
+        "editor.wordBasedSuggestions": false
+    });
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
