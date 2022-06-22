136a399f3bf7bd2f19c81e06701d4686c5ede2a7
Hajin Jang <jb6804@naver.com>
2022-06-23 00:58:50 +0900

    Update extension skeleton files

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,3 @@
-'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
@@ -6,7 +5,7 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    // Disable editor.wordBasedSuggestions, or encoded file will disrupt intellisense
+    // Disable editor.wordBasedSuggestions, or encoded file will disrupt intellisense.
     const config = vscode.workspace.getConfiguration();
     config.update("[pebakery]", {
         "editor.wordBasedSuggestions": false
@@ -14,5 +13,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
